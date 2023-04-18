9f1a606c269a8ec1a708818c0d2f709fc3cbd409
Daniel <dev.danielmarroquin25@gmail.com>
2023-04-18 16:02:01 -0500

    mongo db connect

--- 04-open-jira/pages/api/hello.ts
+++ 04-open-jira/pages/api/hello.ts
@@ -2,12 +2,19 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
-  name: string
+  name: string,
+  age: number,
+  status: boolean
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json(
+    { 
+      name: 'John Doe',
+      age: 23,
+      status: true
+    })
 }
