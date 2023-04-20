698f0cb354b92a2e5aaa883d3734e409f52b8280
Daniel <dev.danielmarroquin25@gmail.com>
2023-04-19 23:57:43 -0500

    mongo data

--- 04-open-jira/context/entries/EntriesProvider.tsx
+++ 04-open-jira/context/entries/EntriesProvider.tsx
@@ -14,26 +14,7 @@ export interface EntriesState {
 
 
 const Entries_INITIAL_STATE: EntriesState = {
-    entries: [
-        {
-            _id: uuidv4(),
-            description: 'Pendiente: Lorem ipsum, dolor sit amet adipisicing elit. Impedit, libero.',
-            status: 'pending',
-            createdAt: Date.now()
-        },
-        {
-            _id: uuidv4(),
-            description: 'In-Progress: Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas',
-            status: 'in-progress',
-            createdAt: Date.now()
-        },
-        {
-            _id: uuidv4(),
-            description: 'Terminadas: ipsum sit amet consectetur adipisicing elit. Quasi, accusantium.',
-            status: 'finished',
-            createdAt: Date.now()
-        }
-    ]
+    entries: []
 }
 
 
---
+++ 04-open-jira/database/seed-data.ts
@@ -0,0 +1,33 @@
+
+
+interface SeedData {
+    entries: SeedEntry[];
+}
+
+
+interface SeedEntry {
+    description: string,
+    createdAt: string,
+    status: number
+}
+
+
+export const seedData: SeedData = {
+    entries: [
+        {
+            description: 'Pendiente: Lorem ipsum, dolor sit amet adipisicing elit. Impedit, libero.',
+            status: 'pending',
+            createdAt: Date.now()
+        },
+        {
+            description: 'In-Progress: Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas',
+            status: 'in-progress',
+            createdAt: Date.now()
+        },
+        {
+            description: 'Terminadas: ipsum sit amet consectetur adipisicing elit. Quasi, accusantium.',
+            status: 'finished',
+            createdAt: Date.now()
+        }
+    ]
+}
