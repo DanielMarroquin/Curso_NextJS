8905d2b65ae69ea4df7c50e0a27e2db3f8e54db1
Daniel <dev.danielmarroquin25@gmail.com>
2023-04-29 00:44:44 -0500

    validate mongo id middleware

--- 04-open-jira/middleware.ts
+++ 04-open-jira/middleware.ts
@@ -1,20 +1,29 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
 // This function can be marked `async` if using `await` inside
 export function middleware(req: NextRequest) {
 
+    if ( req.nextUrl.pathname.startsWith('/api/entries/')) {
+        const id = req.nextUrl.pathname.replace('/api/entries/', '');
+        const checkMongoIDRegExp = new RegExp("^[0-9a-fA-F]{24}$");
+
+        console.log({ req: req.nextUrl.pathname })
+        
+        if ( !checkMongoIDRegExp.test(id) ) {
+            const url = req.nextUrl.clone();
+            url.pathname = '/api/bad-request';
+            
+            return NextResponse.rewrite(url);
+        }   
+    }
 
   return NextResponse.next();
-
-  console.log({ req: req.nextUrl });
 }
 
 export const config = {
-    mather: '/api/:path'
+    matcher: [
+        '/api/entries/:path'
+    ]
 }
 
-// See "Matching Paths" below to learn more
-// export const config = {
-//   matcher: '/about/:path*',
-// }
