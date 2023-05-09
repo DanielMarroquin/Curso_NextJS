e9c54c2f99774d02cb113dbe0c03f685b9114fb5
Daniel <dev.danielmarroquin25@gmail.com>
2023-05-08 23:49:30 -0500

    init process fix

--- 04-open-jira/database/dbEntries.ts
+++ 04-open-jira/database/dbEntries.ts
@@ -3,6 +3,7 @@ import { db } from '.';
 import { Entry, IEntry } from '@/models';
 
 export const getEntryById = async ( id: string ): Promise<IEntry | null>  => {
+    
     if ( !isValidObjectId(id) ) return null;
 
     await db.connect();
